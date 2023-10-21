b7dc6d3498f4ae382098a01bef922722c3ed8a2e
Nazzemax <hvan1998@inbox.ru>
2023-10-21 20:32:56 +0600

    feat(layout): added router and created basic profile component separated several blocks of code to component home

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import { useAppDispatch, useAppSelector } from './app/hooks'
-import Header from './components/partials/Header'
-import Form from './components/shared/Form'
+import { useState } from "react";
+import { useAppSelector } from "./app/hooks/hooks";
+import {BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Profile from "./components/profile/Profile";
+import Home from "./components/home/Home";
 
-const App:React.FC = () => {
-  const dispatch = useAppDispatch()
+const App: React.FC = () => {
   
   return (
-    <>    
-    <div className='bg-[#F9F9FA]'>
-    <Header />
-      <main 
-        className='flex mx-auto' 
-        style={{justifyContent:'center'}}>
-        <Form />
-      </main>
-    </div>
+    <>
+    <Router>
+      <Routes>
+         <Route path="/profile" element={<Profile  />} />
+         <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/partials/Header.tsx
+++ src/components/partials/Header.tsx
@@ -3,7 +3,8 @@ import Logo from '../../assets/logo.svg'
 
 const Header:React.FC = () => {
   return (
-        <div className=" w-screen px-32 mx-auto shadow-header-shadow flex items-center justify-between max-h-16 h-16">
+        <div className=" w-screen px-32 mx-auto shadow-header-shadow
+                       flex items-center justify-between max-h-16 h-16">
             <img src={Logo} alt="logo" />
             <Button />
         </div>
