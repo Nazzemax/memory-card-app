6baab91d357468851ec34c8b514301ee8989fa61
Nazzemax <hvan1998@inbox.ru>
2023-10-31 01:06:41 +0600

    feat:created actions for making request to register route to user be able to logout and to check user cookies

--- src/components/shared/Button.tsx
+++ src/components/shared/Button.tsx
@@ -1,12 +1,16 @@
-const Button: React.FC = () => {
+import { Link } from "react-router-dom";
+
+const Button: React.FC = (): React.JSX.Element => {
   return (
-    <button
-      className="bg-accent-blue hover:bg-blue-700 text-base
+    <Link to="/login">
+      <button
+        className="bg-accent-blue hover:bg-blue-700 text-base
      text-center text-white shadow-btn-shadow
       rounded-3xl w-32 h-9"
-    >
-      Sign in
-    </button>
+      >
+        Sign in
+      </button>
+    </Link>
   );
 };
 
