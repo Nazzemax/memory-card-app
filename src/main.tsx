13f9a51ee94787b2f6b0b257353662c73164f7ca
Nazzemax <hvan1998@inbox.ru>
2023-10-31 00:52:23 +0600

    feat:configured routing based setup

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,35 @@ import App from "./App.tsx";
 import "./index.css";
 import store from "./app/store.ts";
 import { Provider } from "react-redux";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import Form from "./components/shared/form/Form.tsx";
+import ProtectedRoute from "./routes/ProtectedRoute.tsx";
+import Profile from "./components/profile/Profile.tsx";
+import Home from "./components/home/Home.tsx";
+import "react-toastify/dist/ReactToastify.css";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route path="/home" element={<Home />} />
+      <Route path="/register" element={<Form formType="register" />} />
+      <Route path="" element={<ProtectedRoute />}>
+        <Route path="/login" element={<Form formType="login" />} />
+        <Route path="/profile" element={<Profile />} />
+      </Route>
+    </Route>
+  )
+);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
     <React.StrictMode>
-      <App />
+      <RouterProvider router={router} />
     </React.StrictMode>
-    ,
   </Provider>
 );
