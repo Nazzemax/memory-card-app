4e4519fde7ba9fe0c39617eccd25fbb89fd7beb6
Nazzemax <hvan1998@inbox.ru>
2023-11-17 11:11:44 +0600

    feat(redux):implemented slice for getting cards and added initial state for them

---
+++ src/features/cards/CardSlice.ts
@@ -0,0 +1,63 @@
+import { createSlice } from "@reduxjs/toolkit";
+import type { Cards } from "../../app/types";
+import { getCards } from "./CardActions";
+
+const initialState:Cards = {
+    cardPacks:[{
+        _id: '',
+        user_id: '',
+        name: '',
+        cardsCount: 0,
+        created:  '',
+        updated: '',
+        user_name:'',
+    }],
+    cardPacksTotalCount: 0, // количество колод
+    maxCardsCount: 10,
+    minCardsCount: 0,
+    page: 1,
+    pageCount: 4,
+    isLoading:false,
+    error:''
+}
+
+const cardSlice = createSlice({
+    name:'cards',
+    initialState,
+    reducers:{
+        setMaxCards:(state, action) => {
+            state.maxCardsCount = action.payload
+        },
+        setMinCards:(state, action) => {
+            state.minCardsCount = action.payload
+        },
+        setPage:(state, action) => {
+            state.page = action.payload
+        },
+        setItemsPerPage:(state, action) => {
+            state.pageCount = action.payload
+        }
+    },
+    extraReducers:builder => {
+        builder
+            .addCase(getCards.pending, (state => {
+                state.isLoading = true;
+                state.error = ''
+            }))
+            .addCase(getCards.fulfilled, (state, action) => {
+                state.isLoading = false;
+                state.error = ''
+                state.cardPacks = action.payload.cardPacks
+                state.cardPacksTotalCount = action.payload.cardPacksTotalCount
+            })
+            .addCase(getCards.rejected, (state, action) => {
+                state.isLoading = false;
+                state.error = action.error.message || 'Error with getting cards'
+            })
+    }
+
+})
+
+export const {actions, reducer} = cardSlice
+
+export default cardSlice.reducer
