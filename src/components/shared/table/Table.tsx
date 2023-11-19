842b2b73466c3d5004e11abea2be8d3d88aea133
Nazzemax <hvan1998@inbox.ru>
2023-11-20 01:52:16 +0600

    feat(layout):added if no card it will show message

--- src/components/shared/table/Table.tsx
+++ src/components/shared/table/Table.tsx
@@ -31,68 +31,73 @@ const Table: React.FC<{
 
   return (
     <>
-      <table className="mt-6 text-sm not-italic w-full mb-9">
-        <thead className="h-12">
-          <tr className="bg-[#EFEFEF] text-justify font-medium">
-            <th className="pl-9">Name</th>
-            <th>Cards</th>
-            <th>
-              <div className="flex items-center justify-start">
-                <div className="flex items-center cursor-pointer ">
-                  Last Updated
-                  <svg
-                    width="10"
-                    className="ml-1.5 self-middle"
-                    height="6"
-                    viewBox="0 0 10 6"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path d="M5 6L0.669872 0L9.33013 0L5 6Z" fill="black" />
-                  </svg>
-                </div>
-              </div>
-            </th>
-            <th>Created by</th>
-            <th className="pr-9">Actions</th>
-          </tr>
-        </thead>
-        <tbody className="text-[0.8125rem]">
-          {error && (
-            <tr>
-              <td colSpan={5}>Some error</td>
-            </tr>
-          )}
+    <table className="mt-6 text-sm not-italic w-full mb-9 table-auto">
+    <thead className="h-12">
+      <tr className="bg-[#EFEFEF] text-justify font-medium">
+        <th className="pl-9">Name</th>
+        <th className="px-2">Cards</th>
+        <th>
+          <div className="flex items-center justify-start">
+            <div className="flex items-center px-2 cursor-pointer ">
+              Last Updated
+              <svg
+                width="10"
+                className="ml-1.5 self-middle"
+                height="6"
+                viewBox="0 0 10 6"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M5 6L0.669872 0L9.33013 0L5 6Z" fill="black" />
+              </svg>
+            </div>
+          </div>
+        </th>
+        <th>Created by</th>
+        <th className="pr-9">Actions</th>
+      </tr>
+    </thead>
+  <tbody className="text-[0.8125rem]">
+    {isLoading ? (
+      <tr>
+        <td colSpan={5}>
+          <Loader h="min-h-fit max-h-fit" />
+        </td>
+      </tr>
+    ) : error ? (
+      <tr>
+        <td colSpan={5}>Some error {error}</td>
+      </tr>
+    ) : cards.length === 0 ? (
+      <tr>
+        <td colSpan={5} className="text-lg p-4 font-medium text-center">
+          No such card found
+        </td>
+      </tr>
+    ) : (
+      cards.map((card) => (
+        <tr key={card._id} className="h-12">
+          <td className="pl-9">{card.name}</td>
+          <td>{card.cardsCount}</td>
+          <td>{changeDateFormat(card.updated)}</td>
+          <td>{card.user_name}</td>
+          <td className="relative flex justify-center space-x-3 pt-4">
+            <button className="">
+              <img src={LearnSvg} alt="Learn svg icon" />
+            </button>
+            <button className="">
+              <img src={EditCard} alt="Edit svg icon" />
+            </button>
+            <button className="">
+              <img src={DeleteCard} alt="Delete svg icon" />
+            </button>
+          </td>
+        </tr>
+      ))
+    )}
+  </tbody>
+</table>
 
-          {isLoading ? (
-            <tr>
-              <td colSpan={4}>
-                <Loader h="min-h-fit max-h-fit" />
-              </td>
-            </tr>
-          ) : (
-            cards.map((card) => (
-              <tr key={card._id} className="h-12">
-                <td className="pl-9">{card.name}</td>
-                <td>{card.cardsCount}</td>
-                <td>{changeDateFormat(card.updated)}</td>
-                <td>{card.user_name}</td>
-                <td className="relative flex space-x-3 pt-4">
-                  <button className="">
-                    <img src={LearnSvg} alt="Learn svg icon" />
-                  </button>
-                  <button className="">
-                    <img src={EditCard} alt="Edit svg icon" />
-                  </button>
-                  <button className="">
-                    <img src={DeleteCard} alt="Delete svg icon" />
-                  </button>
-                </td>
-              </tr>
-            ))
-          )}
-        </tbody>
-      </table>
 
       <Pagination
         page={page}
@@ -100,6 +105,7 @@ const Table: React.FC<{
         itemsPerPage={pageCount}
         setPage={setPage}
         setItemsPerPage={setItemsPerPage}
+        isLoading={isLoading}
       />
     </>
   );
