b057ec83399cd081da32fd3f02d11116c7440b5a
Nazzemax <hvan1998@inbox.ru>
2023-11-22 23:05:53 +0600

    feat(layout): implemented sort function by name, card count, and by last updated

--- src/components/shared/table/Table.tsx
+++ src/components/shared/table/Table.tsx
@@ -13,6 +13,8 @@ const Table: React.FC<{
   error: string;
   isLoading: boolean;
   cardPacksTotalCount: number;
+  isSorting: string;
+  setSorting: (fieldName: string) => Action;
   setPage: (page: number) => Action;
   setItemsPerPage: (pages: number) => Action;
 }> = ({
@@ -24,80 +26,109 @@ const Table: React.FC<{
   cardPacksTotalCount,
   setPage,
   setItemsPerPage,
+  isSorting,
+  setSorting,
 }) => {
   function changeDateFormat(date: string) {
     return new Date(date).toLocaleDateString("rus");
   }
 
+  const filterBy = async (fieldName: string) => {
+    try {
+      const newSortOrder = isSorting === `0${fieldName}` ? "1" : "0";
+      const sortingParam = `${newSortOrder}${fieldName}`;
+
+      setSorting(sortingParam);
+    } catch (error) {
+      // Handle the error, log it, or show an error message to the user
+      console.error("Error while fetching cards:", error);
+    }
+  };
+
   return (
     <>
-    <table className="mt-6 text-sm not-italic w-full mb-9 table-auto">
-    <thead className="h-12">
-      <tr className="bg-[#EFEFEF] text-justify font-medium">
-        <th className="pl-9">Name</th>
-        <th className="px-2">Cards</th>
-        <th>
-          <div className="flex items-center justify-start">
-            <div className="flex items-center px-2 cursor-pointer ">
-              Last Updated
-              <svg
-                width="10"
-                className="ml-1.5 self-middle"
-                height="6"
-                viewBox="0 0 10 6"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
+      <table className="mt-6 text-sm not-italic w-full mb-9 table-auto">
+        <thead className="h-12">
+          <tr className="bg-[#EFEFEF] text-justify font-medium">
+            <th className="pl-9 cursor-pointer">
+              <button disabled={isLoading} onClick={() => filterBy("name")}>
+                Name
+              </button>
+            </th>
+            <th className="cursor-pointer">
+              <button
+                disabled={isLoading}
+                onClick={() => filterBy("cardsCount")}
               >
-                <path d="M5 6L0.669872 0L9.33013 0L5 6Z" fill="black" />
-              </svg>
-            </div>
-          </div>
-        </th>
-        <th>Created by</th>
-        <th className="pr-9">Actions</th>
-      </tr>
-    </thead>
-  <tbody className="text-[0.8125rem]">
-    {isLoading ? (
-      <tr>
-        <td colSpan={5}>
-          <Loader h="min-h-fit max-h-fit" />
-        </td>
-      </tr>
-    ) : error ? (
-      <tr>
-        <td colSpan={5}>Some error {error}</td>
-      </tr>
-    ) : cards.length === 0 ? (
-      <tr>
-        <td colSpan={5} className="text-lg p-4 font-medium text-center">
-          No such card found
-        </td>
-      </tr>
-    ) : (
-      cards.map((card) => (
-        <tr key={card._id} className="h-12">
-          <td className="pl-9">{card.name}</td>
-          <td>{card.cardsCount}</td>
-          <td>{changeDateFormat(card.updated)}</td>
-          <td>{card.user_name}</td>
-          <td className="relative flex justify-center space-x-3 pt-4">
-            <button className="">
-              <img src={LearnSvg} alt="Learn svg icon" />
-            </button>
-            <button className="">
-              <img src={EditCard} alt="Edit svg icon" />
-            </button>
-            <button className="">
-              <img src={DeleteCard} alt="Delete svg icon" />
-            </button>
-          </td>
-        </tr>
-      ))
-    )}
-  </tbody>
-</table>
-
+                Cards
+              </button>
+            </th>
+            <th>
+              <div className="flex items-center justify-start">
+                <div className="flex items-center cursor-pointer ">
+                  <button
+                    disabled={isLoading}
+                    onClick={() => filterBy("updated")}
+                  >
+                    Last Updated
+                  </button>
+                  <svg
+                    width="10"
+                    className="ml-1.5 self-middle"
+                    height="6"
+                    viewBox="0 0 10 6"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d="M5 6L0.669872 0L9.33013 0L5 6Z" fill="black" />
+                  </svg>
+                </div>
+              </div>
+            </th>
+            <th>Created by</th>
+            <th className="pr-9 pl-6 text-center">Actions</th>
+          </tr>
+        </thead>
+        <tbody className="text-[0.8125rem]">
+          {isLoading ? (
+            <tr>
+              <td colSpan={5}>
+                <Loader h="min-h-fit max-h-fit" />
+              </td>
+            </tr>
+          ) : error ? (
+            <tr>
+              <td colSpan={5}>Some error {error}</td>
+            </tr>
+          ) : cards.length === 0 ? (
+            <tr>
+              <td colSpan={5} className="text-lg p-4 font-medium text-center">
+                No such card pack found
+              </td>
+            </tr>
+          ) : (
+            cards.map((card) => (
+              <tr key={card._id} className="h-12">
+                <td className="pl-9">{card.name}</td>
+                <td>{card.cardsCount}</td>
+                <td>{changeDateFormat(card.updated)}</td>
+                <td>{card.user_name}</td>
+                <td className="relative flex justify-end pr-9 space-x-3 pt-4">
+                  <button className="">
+                    <img src={LearnSvg} alt="Learn svg icon" />
+                  </button>
+                  <button className="">
+                    <img src={EditCard} alt="Edit svg icon" />
+                  </button>
+                  <button className="">
+                    <img src={DeleteCard} alt="Delete svg icon" />
+                  </button>
+                </td>
+              </tr>
+            ))
+          )}
+        </tbody>
+      </table>
 
       <Pagination
         page={page}
