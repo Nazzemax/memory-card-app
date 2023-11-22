4887cc93625569045d569715b2bc89d740e944ac
Nazzemax <hvan1998@inbox.ru>
2023-11-22 23:07:47 +0600

    feat(layout): made input disabled while loading

--- src/components/cards/search/SearchInput.tsx
+++ src/components/cards/search/SearchInput.tsx
@@ -3,7 +3,8 @@ import searchIcon from '../../../assets/searchIcon.svg'
 const SearchInput:React.FC<{
     searchText:string;
      debouncedSearch:(e:React.ChangeEvent<HTMLElement>) => void
-    }> = ({searchText, debouncedSearch}) => {
+     isLoading:boolean
+    }> = ({searchText, debouncedSearch, isLoading}) => {
 
   return (
     <label className='flex-1 relative flex flex-col justify-center text-sm font-medium' htmlFor="search">Search       
@@ -13,6 +14,7 @@ const SearchInput:React.FC<{
       placeholder="Provide your text"
       value={searchText}
       onChange={debouncedSearch}
+      disabled={isLoading}
       className="border-2 outline-none border-gray-200 p-2 pl-10 min-w-min max-w-full mt-2 md:mr-6 lg:mr-6"
     />
     <img className='absolute bottom-3 left-5 w-4 h-4' src={searchIcon} alt="Search icon" />
