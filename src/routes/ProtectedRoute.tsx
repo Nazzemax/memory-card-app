c347d0f8e78df3a458063aed58285261d536eefd
Nazzemax <hvan1998@inbox.ru>
2023-12-17 23:50:59 +0600

    fix(refactor): fixed bug when loggin out

--- src/routes/ProtectedRoute.tsx
+++ src/routes/ProtectedRoute.tsx
@@ -2,22 +2,24 @@ import { Navigate, Outlet } from "react-router-dom";
 import { useAppDispatch, useAppSelector } from "../app/hooks/hooks";
 import { RootState } from "../app/store";
 import { checkCookies } from "../features/auth/AuthActions";
-import { useEffect } from "react";
+import React, { useEffect } from "react";
 
 const ProtectedRoute: React.FC = () => {
 
   const dispatch = useAppDispatch();
   const isAuthenticated = useAppSelector(
-    (state: RootState) => state.auth.isAuthenticated
+    (state: RootState) => state.auth.user.isAuthenticated
   );
+
+  const isLogout = useAppSelector((state:RootState) => state.auth.isLogout)
     
   useEffect(() => {
-    if (!isAuthenticated) {
+    if (!isAuthenticated && isLogout) {
       checkCookies(dispatch);
     }
-  }, [isAuthenticated, dispatch]);
+  }, [isAuthenticated, dispatch, isLogout]);
 
-  return isAuthenticated ? <Outlet /> : <Navigate to="/home" replace />;
+  return isAuthenticated ? <Outlet/> : <Navigate to="/home" />;
 };
 
 export default ProtectedRoute;
