e354170a3802f4d90b84614e16f0aa535fe9128f
Nazzemax <hvan1998@inbox.ru>
2023-12-18 00:16:30 +0600

    feat(;ayout): implemented crud actions for cards and delete for cardpack

--- src/components/shared/table/Table.tsx
+++ src/components/shared/table/Table.tsx
@@ -1,25 +1,34 @@
-import LearnSvg from "../../../assets/learnCard.svg";
-import EditCard from "../../../assets/editCard.svg";
-import DeleteCard from "../../../assets/deleteCard.svg";
-import Loader from "../../utils/Loader";
 import Pagination from "./Pagination";
 import { Action } from "@reduxjs/toolkit";
-import { CardPacks } from "../../../app/types";
+import { CardPacks, CardsList } from "../../../app/types";
+import { useAppSelector } from "../../../app/hooks/hooks";
+import { changeDateFormat } from "../../utils/FormatDate";
+import React from "react";
+import { useActions } from "../../../app/hooks/useActions";
+import TableHeader from "./TableHeader";
+import TableBody from "./TableBody";
 
 const Table: React.FC<{
-  cards: CardPacks;
+  cardsPacks?: CardPacks;
+  cardsPack_id?:string;
+  cards?:CardsList;
   page: number;
   pageCount: number;
   error: string;
   isLoading: boolean;
-  cardPacksTotalCount: number;
+  cardPacksTotalCount?: number;
+  cardsTotalCount?:number;
   isSorting: string;
   setSorting: (fieldName: string) => Action;
   setPage: (page: number) => Action;
   setItemsPerPage: (pages: number) => Action;
+  toggleState?:string;
 }> = ({
+  cardsPacks,
+  cardsPack_id,
   cards,
   page,
+  cardsTotalCount,
   pageCount,
   error,
   isLoading,
@@ -28,10 +37,9 @@ const Table: React.FC<{
   setItemsPerPage,
   isSorting,
   setSorting,
+  toggleState,
 }) => {
-  function changeDateFormat(date: string) {
-    return new Date(date).toLocaleDateString("rus");
-  }
+  const { user } = useAppSelector((state) => state.auth.user)
 
   const filterBy = async (fieldName: string) => {
     try {
@@ -45,94 +53,93 @@ const Table: React.FC<{
     }
   };
 
+  const {
+    cardQuestion,
+    isSorted,
+    minGrade,
+    maxGrade
+  } = useAppSelector((state) => state.cards);
+
+  const { deleteCardPack, getPackCards, updateCardPack, getCard, updateCard, deleteCard } = useActions()
+
+  const handleRemovePack = (_id:string, user:string) => {
+    deleteCardPack(_id)
+    if(toggleState === 'My') {
+      getPackCards({user_id:user})
+    } else if (toggleState === 'All') {
+      getPackCards({})
+    }
+  }
+
+  const updatePack = async (id:string, name:string, user:string) => {
+      await updateCardPack({_id:id, name})
+    if(toggleState === 'My') {
+      await getPackCards({user_id:user})
+    } else if (toggleState === 'All') {
+      await getPackCards(
+        {
+          page,
+          pageCount,
+        }
+      )
+    }
+  }
+
+const removeCard = (id: string) => {
+    deleteCard(id);
+if(cardsTotalCount && cards) {
+  if (cardsTotalCount > 0 && cards.length > 0) {
+    getCard({
+      cardsPack_id: cardsPack_id,
+      cardQuestion,
+      pageCount,
+      page,
+      min: minGrade,
+      max: maxGrade,
+      sortCards: isSorted,
+    })
+  }
+}
+};
+
+  const updatingCard = async (id:string, question:string, answer:string) => {
+    await updateCard({_id: id, question, answer})
+
+    await getCard({
+      cardsPack_id: cardsPack_id,
+      cardQuestion,
+      pageCount,
+      page,
+      min: minGrade,
+      max: maxGrade,
+      sortCards: isSorted,
+    })
+  
+ }
+
   return (
     <>
       <table className="mt-6 text-sm not-italic w-full mb-9 table-auto">
         <thead className="h-12">
-          <tr className="bg-[#EFEFEF] text-justify font-medium">
-            <th className="pl-9 cursor-pointer">
-              <button disabled={isLoading} onClick={() => filterBy("name")}>
-                Name
-              </button>
-            </th>
-            <th className="cursor-pointer">
-              <button
-                disabled={isLoading}
-                onClick={() => filterBy("cardsCount")}
-              >
-                Cards
-              </button>
-            </th>
-            <th>
-              <div className="flex items-center justify-start">
-                <div className="flex items-center cursor-pointer ">
-                  <button
-                    disabled={isLoading}
-                    onClick={() => filterBy("updated")}
-                  >
-                    Last Updated
-                  </button>
-                  <svg
-                    width="10"
-                    className="ml-1.5 self-middle"
-                    height="6"
-                    viewBox="0 0 10 6"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path d="M5 6L0.669872 0L9.33013 0L5 6Z" fill="black" />
-                  </svg>
-                </div>
-              </div>
-            </th>
-            <th>Created by</th>
-            <th className="pr-9 pl-6 text-center">Actions</th>
-          </tr>
+         <TableHeader cardsPacks={cardsPacks || ''} filterBy={filterBy} isLoading={isLoading} />
+  
         </thead>
-        <tbody className="text-[0.8125rem]">
-          {isLoading ? (
-            <tr>
-              <td colSpan={5}>
-                <Loader h="min-h-fit max-h-fit" />
-              </td>
-            </tr>
-          ) : error ? (
-            <tr>
-              <td colSpan={5}>Some error {error}</td>
-            </tr>
-          ) : cards.length === 0 ? (
-            <tr>
-              <td colSpan={5} className="text-lg p-4 font-medium text-center">
-                No such card pack found
-              </td>
-            </tr>
-          ) : (
-            cards.map((card) => (
-              <tr key={card._id} className="h-12">
-                <td className="pl-9">{card.name}</td>
-                <td>{card.cardsCount}</td>
-                <td>{changeDateFormat(card.updated)}</td>
-                <td>{card.user_name}</td>
-                <td className="relative flex justify-end pr-9 space-x-3 pt-4">
-                  <button className="">
-                    <img src={LearnSvg} alt="Learn svg icon" />
-                  </button>
-                  <button className="">
-                    <img src={EditCard} alt="Edit svg icon" />
-                  </button>
-                  <button className="">
-                    <img src={DeleteCard} alt="Delete svg icon" />
-                  </button>
-                </td>
-              </tr>
-            ))
-          )}
-        </tbody>
+        <TableBody
+          cardsPacks={cardsPacks} 
+          cards={cards}
+          isLoading={isLoading}
+          error={error} 
+          handleRemovePack={handleRemovePack}
+          updatePack={updatePack}
+          removeCard={removeCard}
+          user={user}
+          changeDateFormat={changeDateFormat}
+          updatingCard={updatingCard} />
       </table>
 
       <Pagination
         page={page}
-        total={cardPacksTotalCount}
+        total={cardPacksTotalCount || cardsTotalCount || 0}
         itemsPerPage={pageCount}
         setPage={setPage}
         setItemsPerPage={setItemsPerPage}
