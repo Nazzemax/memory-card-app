70fd34f6e099c0d14426ecafe144ad882fd8f88a
Nazzemax <hvan1998@inbox.ru>
2023-12-18 00:11:49 +0600

    feat(layout): added for checking is user is authenticated

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,6 +1,22 @@
+import { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "../../app/hooks/hooks";
+import { RootState } from "../../app/store";
 import Form from "../shared/form/Form";
+import { checkCookies } from "../../features/auth/AuthActions";
 
 const Home: React.FC = (): React.JSX.Element => {
+
+  const dispatch = useAppDispatch();
+  const isAuthenticated = useAppSelector(
+    (state: RootState) => state.auth.user.isAuthenticated
+  );
+    
+  useEffect(() => {
+    if (!isAuthenticated) {
+      checkCookies(dispatch);
+    }
+  }, [isAuthenticated, dispatch]);
+
   return (
     <main className="flex mx-auto px-32" style={{ justifyContent: "center" }}>
       <Form formType="login" />
